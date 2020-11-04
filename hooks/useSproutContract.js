bd7849cac884a653cd7b1e871c2377191b56960f
richance <33940018+richance@users.noreply.github.com>
2020-11-04 10:15:41 +0100

    Update useSproutContract.js

--- hooks/useSproutContract.js
+++ hooks/useSproutContract.js
@@ -2,6 +2,6 @@ import sproutAbi from "../constants/abi/sproutContract.json";
 import useContract from "./useContract";
 
 export default function usePoolContract() {
-  return useContract("0x9c539380C2c1c88E25dEB6275b24447D6059F7E8",sproutAbi)
+  return useContract("0x42b54830BCBb0A240Aa54cd3f8d1A4DB00851fE3",sproutAbi)
 }
 
